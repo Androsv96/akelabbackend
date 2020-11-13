c14c601a88689a73b1d41c28c203cb3708841872
androsv <androsg93@hotmail.com>
2020-11-13 14:48:42 -0500

    solved issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const cors = require('cors')
 const moviesJson = require('./Utils/Assets/movies.json');
-const AKELAB = require('./Utils/Constants');
+const { AKELAB } = require('./Utils/Constants');
 
 app.use(cors())
 
@@ -10,12 +10,12 @@ app.get('/', (req, res) => {
     try {
         const { Akelab } = req.query;
         if (!Akelab) {
-            res.sendStatus(404);
+            res.status(404);
             return res.json({ error: true, errorMsg: "Por favor envíe el código Akelab." });
         }
 
         if (AKELAB !== Akelab) {
-            res.sendStatus(403);
+            res.status(403);
             return res.json({ error: true, errorMsg: "Por favor envíe el código correcto de Akelab." });
         }
 
@@ -24,9 +24,11 @@ app.get('/', (req, res) => {
 
     } catch (exception) {
         console.log(exception)
+        res.status(500);
+        return res.json({ error: true, errorMsg: "Hubo un error" });
     }
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 8000, () => {
     console.log('Example app listening on port 8000!')
 });
